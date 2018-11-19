c157eedbe0640723bb255f30fbfca0c771ca51c9
audryus <audryus@gmail.com>
2018-11-19 18:49:18 -0200

    Adding some interpreter for the AIML pattern.

--- 007/aiml/loader.py
+++ 007/aiml/loader.py
@@ -1,23 +1,56 @@
 import aiml
 import os
+import glob
+import re
 import pyttsx3
+import xml.etree.ElementTree as ET
+import spacy
+import numpy as np
+import timeit
+import random
 
+# Load the spacy model: nlp
+nlp = spacy.load('en_core_web_lg')
 current_file = os.path.abspath(os.path.dirname(__file__))
+
+files = glob.glob(current_file + '/*.aiml')
+# questions = []
+# qna = []
+questions = {}
+
+
+def answer(message):
+    start = timeit.default_timer()
+    resp = nlp(message)
+    _a = "Sorry. I don't understand."
+    result = 0
+    qList = set()
+    for k, v in questions.items():
+        _s = v.similarity(resp)
+        if _s > result:
+            result = _s
+            qList.clear()
+            qList.add(k)
+        elif _s == result:
+            qList.add(k)
+    _q = random.choice(list(qList))
+    _a = kernel.respond(questions[_q])
+    return _a
+
+
+for f in files[:2]:
+    tree = ET.parse(f)
+    root = tree.getroot()
+    for cat in root.findall('category'):
+        _p = cat.find('pattern').text
+        questions[_p] = nlp(_p)
+
 
 kernel = aiml.Kernel()
 kernel.setBotPredicate("name", "Chief")
 
 kernel.learn(current_file + "/startup.xml")
 kernel.respond("load aiml")
-engine = pyttsx3.init()
-
-
-def answer(question):
-    ans = kernel.respond(question)
-    engine.say(ans)
-    engine.runAndWait()
-    return ans
-
 
 while True:
     print(answer(input("> ")))
--- 007/eliza.py
+++ 007/eliza.py
@@ -1,8 +1,13 @@
+import xml.etree.ElementTree as ET
 import re
 import os
 import sqlite3
 import nltk
-from nltk.draw.tree import TreeView
+import numpy as np
+import spacy
+import timeit
+# Load the spacy model: nlp
+nlp = spacy.load('en_core_web_lg')
 
 
 def replace_pronouns(message):
@@ -39,15 +44,68 @@ for row in result:
         answers.append(row[1])
 
 sentence = questions[1]
-
+sentences = questions[:1]
 print(sentence)
 print(answers[1])
 
 # tokens = nltk.word_tokenize(sentence)
 tokens = nltk.sent_tokenize(sentence)
+token = tokens[-1]
 print(tokens)
 tagged = nltk.pos_tag(tokens)
 print(tagged)
 
 entities = nltk.chunk.ne_chunk(tagged)
-TreeView(entities)._cframe.print_to_file(current_file + '/output.ps')
+
+
+def extract_entities(message):
+    # Create a dict to hold the entities
+    doc = nlp(message)
+    for token in doc:
+        print("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}".format(
+            token.text,
+            token.idx,
+            token.lemma_,
+            token.is_punct,
+            token.is_space,
+            token.shape_,
+            token.pos_,
+            token.tag_
+        ))
+
+
+# print(extract_entities(sentence))
+# print(extract_entities(answers[1]))
+
+
+targets = []
+qna = []
+tree = ET.parse(current_file + '/aiml/bot.aiml')
+root = tree.getroot()
+
+for country in root.findall('category'):
+    _p = country.find('pattern').text
+    _t = country.find('template').text
+    if _t:
+        targets.append(nlp(_p))
+        qna.append(_t)
+
+print(len(targets))
+
+
+def answer(message):
+    start = timeit.default_timer()
+    resp = nlp(message)
+    _a = "Sorry. I don't understand."
+    result = np.zeros(len(targets))
+    for i, t in enumerate(targets):
+        result[i] = t.similarity(resp)
+    _ima = np.nanargmax(result)
+    if len(qna) >= _ima:
+        _a = qna[_ima]
+    print('Time: ', timeit.default_timer() - start)
+    return _a
+
+
+while True:
+    print(answer(input("> ")))
